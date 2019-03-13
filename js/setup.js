69655ab4db397fbbcd2bba9e9ec5a4896855e284
dkrech07 <dkrech07@gmail.com>
2019-03-13 21:53:38 +0500

    добавил массивы имен/фамилий/цветов

--- js/setup.js
+++ js/setup.js
@@ -6,7 +6,17 @@ document.querySelector('.setup-similar').classList.remove('hidden');
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
-var WIZARD_NAMES = ['Дамблдор', 'Волдеморт', 'Доктор Стрендж', 'Гарри Поттер'];
+// Массив имен
+var WIZARD_NAMES = ['Иван', 'Хуан Себестьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+
+// Массив фамилий
+var WIZARD_SECOND_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+
+// Массив цветов мантии
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43,107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+
+// Массив цвета глаз
+var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 
 var wizards = [{
     name: WIZARD_NAMES[0],
