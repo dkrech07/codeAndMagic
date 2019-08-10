b2c1afcd76514b80578948ddd8af39f7e7ab1197
Дмитрий Кречетников <dkrech07@gmail.com>
2019-08-10 20:18:54 +0500

    добавил загрузку магов с сервера

--- js/setup.js
+++ js/setup.js
@@ -1,47 +1,42 @@
 (function() {
-  var WIZARD_NAMES = ['Дамблдор', 'Волдеморт', 'Доктор Стрендж', 'Гарри Поттер'];
-
   var userDialog = document.querySelector('.setup');
   userDialog.classList.remove('hidden');
 
   var similarListElement = document.querySelector('.setup-similar-list');
-  var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
-  var wizards = [{
-      name: WIZARD_NAMES[0],
-      coatColor: 'rgb(241,43,107)'
-    },
-    {
-      name: WIZARD_NAMES[1],
-      coatColor: 'rgb(215,210,55)'
-    },
-    {
-      name: WIZARD_NAMES[2],
-      coatColor: 'rgb(101,137,164)'
-    },
-    {
-      name: WIZARD_NAMES[3],
-      coatColor: 'rgb(127,127,127)'
-    }
-  ];
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
   var renderWizard = function(wizard) {
     var wizardElement = similarWizardTemplate.cloneNode(true);
 
     wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
 
     return wizardElement;
-  }
+  };
 
-  window.load(function(wizards) {
+  var successHandler = function(wizards) {
     var fragment = document.createDocumentFragment();
 
-    for (var i = 0; i < wizards.length; i++) {
+    for (var i = 0; i < 4; i++) {
       fragment.appendChild(renderWizard(wizards[i]));
     }
     similarListElement.appendChild(fragment);
 
-    userDialog.querySelector('.setup-similar'), classList.remove('hidden');
-  });
+    userDialog.querySelector('.setup-similar').classList.remove('hidden');
+  };
+
+  var errorHandler = function(errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-aling: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fintSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  window.load(successHandler, errorHandler);
 })();
